8ccc553a39b9b92db4aace19b1314461ee4c5d16
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-04-03 12:50:43 -0300

    feat: change route and server files

--- broker/main.go
+++ broker/main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
-	"broker/server"
+	"broker/routes"
 	"broker/utils"
 	"fmt"
+	"net/http"
 	"strings"
 
 	"github.com/fatih/color"
+	"github.com/gorilla/mux"
+)
+
+const (
+	defaultPort = 8080
 )
 
 func main() {
 	showWelcomeMsg()
-	server.Init()
+
+	handleServer()
+}
+
+func handleServer() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", routes.GetRootHandler).Methods("GET")
+	r.HandleFunc("/message", routes.PostMessageHandler).Methods("POST")
+	r.HandleFunc("/sensor", routes.CreateSensorHandler).Methods("POST")
+	r.HandleFunc("/sensor", routes.FindAllSensorsHandler).Methods("GET")
+
+	fmt.Println("Server started on port", defaultPort)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", defaultPort), r)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
 
 func showWelcomeMsg() {
