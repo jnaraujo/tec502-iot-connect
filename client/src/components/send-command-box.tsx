f28e6e182b32ee9cf2f9a7072902e91a5a12ace9
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-04-05 14:08:18 -0300

    feat: improved send command box

--- client/src/components/send-command-box.tsx
+++ client/src/components/send-command-box.tsx
@@ -5,32 +5,34 @@ import { Textarea } from "@/components/ui/textarea"
 
 export function SendCommandBox() {
   return (
-    <article className="bg-background flex w-[350px] flex-col space-y-2 rounded-lg border p-6">
+    <article className="bg-background flex flex-col space-y-2 rounded-lg border p-6">
       <h2 className="text-lg font-medium text-zinc-900">Enviar comando</h2>
 
       <form className="flex flex-1 flex-col justify-between gap-4">
         <div className="flex flex-col gap-4">
-          <div className="space-y-1">
-            <Label htmlFor="command">Sensor ID:</Label>
-            <Input
-              id="command"
-              name="command"
-              placeholder="Ex: temp1"
-              className="col-span-3"
-              required
-            />
+          <div className="grid grid-cols-2 gap-4">
+            <div className="space-y-1">
+              <Label htmlFor="command">Sensor ID:</Label>
+              <Input
+                id="command"
+                name="command"
+                placeholder="Ex: temp1"
+                className="col-span-3"
+                required
+              />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="command">Comando:</Label>
+              <Input
+                id="command"
+                name="command"
+                placeholder="Ex: get_time"
+                className="col-span-3"
+                required
+              />
+            </div>
           </div>
 
-          <div className="space-y-1">
-            <Label htmlFor="command">Comando:</Label>
-            <Input
-              id="command"
-              name="command"
-              placeholder="Ex: get_time"
-              className="col-span-3"
-              required
-            />
-          </div>
           <div className="space-y-1">
             <Label htmlFor="content">Conteúdo:</Label>
             <Textarea
--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,17 +1,16 @@
-import { SendCommandBox } from "@/components/send-command"
+import { SendCommandBox } from "@/components/send-command-box"
 import { SensorListBox } from "@/components/sensor-list-box"
 import { SensorResponseListBox } from "@/components/sensor-response-list-box"
 
 export function Home() {
   return (
     <main className="container grid h-screen grid-cols-[1fr_400px] gap-6 py-6">
-      <section className="grid h-fit grid-cols-2">
+      <section className="grid h-fit">
         <SendCommandBox />
       </section>
 
       <section className="grid h-full grid-rows-2 gap-4 overflow-auto">
         <SensorResponseListBox />
-
         <SensorListBox />
       </section>
     </main>
