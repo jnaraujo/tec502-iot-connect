17c6ca65e3362785a7380d46d4c1541b4011d754
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-04-05 11:03:27 -0300

    fix: prettier

--- client/.prettierrc.cjs
+++ client/.prettierrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   "root": true,
   "plugins": [
-    import("prettier-plugin-tailwindcss"),
-    import("@ianvs/prettier-plugin-sort-imports")
+    import("@ianvs/prettier-plugin-sort-imports"),
+    import("prettier-plugin-tailwindcss")
   ],
   "printWidth": 80,
   "tabWidth": 2,
