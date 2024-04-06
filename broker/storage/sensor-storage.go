72fe7e1d71465c82e670a3742e1541e552e973ce
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-04-06 00:54:18 -0300

    refactor: sensor storage

--- broker/storage/sensor-storage.go
+++ broker/storage/sensor-storage.go
@@ -1,36 +1,26 @@
 package storage
 
-import (
-	"sync"
-)
-
 type SensorStorage struct {
 	addrs map[string]string
-	mutex sync.RWMutex
 }
 
 var sensorStorage *SensorStorage
 
 func GetSensorStorage() *SensorStorage {
 	if sensorStorage == nil {
-		sensorStorage = &SensorStorage{}
-		sensorStorage.addrs = make(map[string]string)
+		sensorStorage = &SensorStorage{
+			addrs: make(map[string]string),
+		}
 	}
 
 	return sensorStorage
 }
 
 func (s *SensorStorage) AddSensor(name string, addr string) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-
 	s.addrs[addr] = name
 }
 
 func (s *SensorStorage) GetSensors() []map[string]string {
-	s.mutex.RLock()
-	defer s.mutex.RUnlock()
-
 	var sensors []map[string]string = []map[string]string{}
 
 	for addr, name := range s.addrs {
@@ -44,9 +34,6 @@ func (s *SensorStorage) GetSensors() []map[string]string {
 }
 
 func (s *SensorStorage) FindSensorAddrByName(name string) string {
-	s.mutex.RLock()
-	defer s.mutex.RUnlock()
-
 	for addr, sensorName := range s.addrs {
 		if sensorName == name {
 			return addr
@@ -57,16 +44,10 @@ func (s *SensorStorage) FindSensorAddrByName(name string) string {
 }
 
 func (s *SensorStorage) DeleteSensorByAddress(addr string) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-
 	delete(s.addrs, addr)
 }
 
 func (s *SensorStorage) FindSensorNameByAddress(addr string) string {
-	s.mutex.RLock()
-	defer s.mutex.RUnlock()
-
 	for sensorAddr, sensorName := range s.addrs {
 		if sensorAddr == addr {
 			return sensorName
