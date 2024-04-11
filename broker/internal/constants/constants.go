eb8cfe4df601bc1c1a192475788ecd859319a687
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-04-11 20:26:12 -0300

    refactor: storage package imports in HTTP routes

---
+++ broker/internal/constants/constants.go
@@ -0,0 +1,7 @@
+package constants
+
+import "time"
+
+const (
+	MaxTimeToBeConsideredOnline = 5 * time.Second
+)
--- broker/internal/http/routes/find-all-sensors.go
+++ broker/internal/http/routes/find-all-sensors.go
@@ -1,16 +1,13 @@
 package routes
 
 import (
+	"broker/internal/constants"
 	"broker/internal/storage/responses"
 	"broker/internal/storage/sensors"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
-)
-
-const (
-	maxTimeSinceLastResponse = 10 * time.Second
 )
 
 type sensorWithOnlineStatus struct {
@@ -22,13 +19,12 @@ func FindAllSensorsHandler(c *gin.Context) {
 	sensors := sensors.FindSensors()
 
 	sensorsWithStatus := []sensorWithOnlineStatus{}
-
 	for _, sensor := range sensors {
 		resp := responses.FindBySensorId(sensor.Id)
 		isOnline := true
 
 		if resp.UpdatedAt.Time.IsZero() ||
-			time.Since(resp.UpdatedAt.Time) > maxTimeSinceLastResponse {
+			time.Since(resp.UpdatedAt.Time) > constants.MaxTimeToBeConsideredOnline {
 			isOnline = false
 		}
 
